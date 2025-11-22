6daed8690dc2ed13e2a880fd7d28cf3736529c67
hrkzz <hiro.suzuki.15@gmail.com>
2025-11-23 01:47:38 +0900

    fix

--- app.py
+++ app.py
@@ -458,7 +458,7 @@ def render_citizen_tab():
 def main():
     apply_custom_styles()
     st.title("Civic Reach")
-    st.markdown("Identifying 'Sludge' in government services using Behavioral Science and Generative AI. Based on the OECD report *'Fixing Frictions: ‘Sludge audits’ around the world'*.Details of the evaluation criteria can be found [here](https://github.com/hrkzz/civic-reach/blob/main/methodology.md)")
+    st.markdown("Identifying 'Sludge' in government services using Behavioral Science and Generative AI. Based on the OECD report *'Fixing Frictions: ‘Sludge audits’ around the world'*. Details of the evaluation criteria can be found [here](https://github.com/hrkzz/civic-reach/blob/main/methodology.md)")
     tab_official_notice, tab_official_flyer, tab_citizen = st.tabs(["【Officials】 Notice Audit", "【Officials】 Flyer Audit", "【Citizens】 Doc Decipher"])
     with tab_official_notice: render_tab_content("notice")
     with tab_official_flyer: render_tab_content("flyer")
